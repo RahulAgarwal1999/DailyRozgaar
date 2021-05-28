956c4c997fd62cb766dbb7f1a38045d266c3eb57
RahulAgarwal31101999 <rahul.agarwal31101999@gmail.com>
2021-05-28 16:45:31 +0530

    our team page

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('our_team',views.our_team,name='our_team'),
 ]
--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'pages/index.html')
+
+def our_team(request):
+    return render(request,'pages/our_team.html')
