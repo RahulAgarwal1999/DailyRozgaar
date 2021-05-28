532d5ffb7f4d1b91a297907e8f4ff3f3df97c0a6
mhjn95 <aayushmahajan950@gmail.com>
2021-05-28 16:42:48 +0530

    Update

--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'pages/index.html')
+
+def customerindex(request):
+    return render(request,'pages/customerindex.html')
