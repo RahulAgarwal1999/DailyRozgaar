3601d6337c62901d0cfa63ff31fce41c5d216632
Rahul Agarwal <58648955+RahulAgarwal1999@users.noreply.github.com>
2022-04-27 09:42:45 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -130,6 +130,6 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
-EMAIL_HOST_USER = 'aayushmahajan950@gmail.com'
+EMAIL_HOST_USER = 'XXXXXXXXXXX@gmail.com'
 EMAIL_HOST_PASSWORD = 'ksitltgsqbkogrvu'
 EMAIL_USE_TLS = True
